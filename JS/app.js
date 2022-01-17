5fa7241aa129d7d17575f4fa3f84f39e46159f03
akki0-0 <37176420+akki0-0@users.noreply.github.com>
2022-01-18 01:16:23 +0530

    Update app.js

---
+++ JS/app.js
@@ -0,0 +1,67 @@
+showNode();
+
+//add notes to localStorage
+
+let addBtn = document.getElementById('addBtn')
+addBtn.addEventListener('click', function (e) {
+    //Get value provided in text area
+    let addTxt = document.getElementById("addTxt")
+    getNotes();
+    //push is used with array, hence notesObj becomes array
+    notesObj.push(addTxt.value);
+    //setting the key "notes" with the value in text area
+    localStorage.setItem("notes", JSON.stringify(notesObj))
+    //removing the value from text area
+    addTxt.value = ' '
+    showNode();
+})
+
+function showNode() {
+    getNotes();
+
+    let html = ""
+    //iterating through each element of key "notes" which is an array of notes
+    notesObj.forEach((element,index) => {
+        //using html += since not using + will replace the created card with latest note
+        //Giving the delete button id of index to remove the specific note only
+    html += `
+    <div class="card mx-2 my-2" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Note : ${index + 1}</h5>
+            <p class="card-text">${element}</P>
+            <button id="${index}" onclick="deleteNode(this.id)" class="btn btn-primary">Delete</button>
+        </div>
+    </div>
+    ` 
+    });
+
+    let notesElement = document.getElementById("notes")
+    if(notesObj.length!=0){
+        notesElement.innerHTML = html
+    }
+    else{
+        notesElement.innerHTML=`Nothing to show!`
+    }
+}
+
+function deleteNode(index){
+    getNotes();
+    //Slice removes element from index position, the number of elements to remove is the 2nd parameter
+    notesObj.splice(index,1)
+    //Updating the local storage since we are just making changes in notesObj
+    localStorage.setItem("notes", JSON.stringify(notesObj))
+    //calling showNode to display latest changes
+    showNode();
+}
+
+function getNotes(){
+    //getting notes from local storage
+    let notes = localStorage.getItem('notes')
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        //parsing the array into String 
+        notesObj = JSON.parse(notes)
+    }
+}
